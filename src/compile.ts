94c1a8c19fb3e747910aeb0a0edeca584932283e
Jungwoo An <o1o9814@naver.com>
2020-10-10 14:10:10 +0900

    feat: Add compile
    Parse vue file and compile script, template blocks

---
+++ src/compile.ts
@@ -0,0 +1,47 @@
+import { TranspileOptions } from 'typescript';
+import { parse } from '@vue/compiler-sfc';
+
+import compileScript from './compile-script';
+import compileTemplate from './compile-template';
+
+function compile({
+  source,
+  filename,
+  tsConfig,
+}: {
+  source: string;
+  filename: string;
+  tsConfig: TranspileOptions;
+}) {
+  const { descriptor, errors } = parse(source, {
+    // TODO :: Sourcemap
+    sourceMap: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.join('\n'));
+  }
+
+  const compiledScript = descriptor.script
+    ? compileScript({
+        block: descriptor.script,
+        tsConfig,
+      })
+    : '';
+
+  const compiledTemplate = descriptor.template
+    ? compileTemplate({
+        block: descriptor.template,
+        filename,
+        tsConfig,
+      })
+    : '';
+
+  return `
+    ${compiledScript};
+    ${compiledTemplate};
+    exports.default.render = render;
+  `;
+}
+
+export default compile;
